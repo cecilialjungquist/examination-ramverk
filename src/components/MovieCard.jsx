bd7a600a33fdd2b932c7a53a41bcf477081365c8
cecilialjungquist <111697322+cecilialjungquist@users.noreply.github.com>
2023-04-27 16:00:22 +0200

    checking validation on links

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,13 +1,30 @@
 import { Link } from 'react-router-dom';
 import './MovieCard.scss';
+import defaultImage from '../../public/movieposter.svg';
 
 function MovieCard({ movie }) {
 
-    return (  
+    // Check if link is ok
+    function checkSrc(src, isImage) {
+        let checkedSrc;
+        try {
+            checkedSrc = new URL(src);
+        } catch (error) {
+            console.log("Broken link", error);
+            if (isImage) {
+                return defaultImage;
+            } else {
+                return 'https://www.imdb.com/';
+            }
+        }
+        return src;
+    }
+
+    return (
         <article>
             <Link to={`/${movie.id}`} state={movie}>
                 <figure>
-                    <img src={movie.img} alt="Movie poster" />
+                    <img src={checkSrc(movie.img, true)} alt="Movie poster" />
                 </figure>
             </Link>
             <h3>{movie.title}</h3>
@@ -16,7 +33,7 @@ function MovieCard({ movie }) {
                 <p>{movie.director}</p>
                 <p> <em>&#9733;</em> {movie.rating}</p>
             </section>
-                <a href={movie.imdb} target="_blank">Link to IMDB</a>
+            <a href={checkSrc(movie.imdb)} target="_blank">Link to IMDB</a>
         </article>
     );
 }
