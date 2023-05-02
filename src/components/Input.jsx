2313155c69d56f033618b59b088803bb5325aa9c
cecilialjungquist <111697322+cecilialjungquist@users.noreply.github.com>
2023-05-02 14:27:18 +0200

    Added favicon

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,10 +1,15 @@
 import './Input.scss';
 
 function Input({ name, type, handleChange, value }) {
+    let label = name;
+
+    if (name === 'img') {
+        label = 'image'
+    } 
 
     return (  
         <section className="input">
-            <label>{name}</label>
+            <label>{label}</label>
             <input name={name} type={type} onChange={handleChange} value={value} />
         </section>
     );
