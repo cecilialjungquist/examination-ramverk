dcb1a0a0b960756f3571071ebb86534dd0581964
cecilialjungquist <111697322+cecilialjungquist@users.noreply.github.com>
2023-05-05 14:53:08 +0200

    Cleanup

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 ## Examinaton ramverk
+
+Ract, Redux Toolkit och Sass.
+Sidan är designad för mobile men fungerar även bra på desktop.
+
+Individuell examination för kursen Ramverk, VT22.
--- src/store/slices/moviesSlice.js
+++ src/store/slices/moviesSlice.js
@@ -7,21 +7,17 @@ export const moviesSlice = createSlice({
     initialState,
     reducers: {
         setAllMovies: (state, action) => {
-            console.log('Setting all movies');
             return action.payload;
         },
         addMovie: (state, action) => {
             state.unshift(action.payload);
-            console.log('Adding movie', action.payload);
         },
         deleteMovie: (state, action) => {
             const index = state.findIndex(movie => movie.id === action.payload.id);
-            console.log('Deleting movie', action.payload);
             state.splice(index, 1);
         },
         editMovie: (state, action) => {
             const index = state.findIndex(movie => movie.id === action.payload.id);
-            console.log('Editing movie', action.payload);
             state.splice(index, 1, action.payload);
         }
     }
